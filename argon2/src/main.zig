9fb29fcf7f6fafc670b08bd6d2ec43d16d9b6e68
Julian Kliebhan <hallo@jkliebhan.de>
2026-02-08 19:42:37 +0100

    reading pw from stdin

--- argon2/src/main.zig
+++ argon2/src/main.zig
@@ -4,9 +4,13 @@ const argon2 = std.crypto.pwhash.argon2;
 pub fn main() !void {
     const alloc = std.heap.page_allocator;
     var buffer: [255]u8 = undefined;
-    const hash = try setPassword(alloc, "Swordfish00", &buffer);
-    std.debug.print("pw1: {s}\n", .{hash});
-    if(argon2.strVerify(hash, "Swordfish00", .{ .allocator = alloc})) |_| {
+    var readBuffer: [255]u8 = undefined;
+
+    std.debug.print("Enter your password: ", .{});
+    const pw =  try readFromStdIn(&readBuffer);
+    const hash = try hashPassword(alloc, pw, &buffer);
+    
+    if(argon2.strVerify(hash, pw, .{ .allocator = alloc})) |_| {
         std.debug.print("success", .{});
     } else |err| {
         switch (err) {
@@ -16,14 +20,12 @@ pub fn main() !void {
     }
 }
 
-pub fn setPassword(allocator: std.mem.Allocator, password: []const u8, buffer: *[255]u8) ![]const u8 {
+pub fn hashPassword(allocator: std.mem.Allocator, password: []const u8, buffer: *[255]u8) ![]const u8 {
     const params = argon2.Params.owasp_2id;
     const mode =   argon2.Mode.argon2id;
     const hash =  try argon2.strHash(password, .{ .allocator = allocator, .params = params, .mode = mode}, buffer);
-    std.debug.print("pw1: {s}\n", .{hash});
     return hash;
 }
-
 
 pub fn readFromStdIn(buffer: []u8) ![]u8 {
     const stdin = std.fs.File.stdin();
